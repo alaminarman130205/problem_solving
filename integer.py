7a6f7a2ee0ed6569807b8ca15ad180594c4c6811
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-24 12:24:38 +0600

    same problem in javascript and python as well as

---
+++ integer.py
@@ -0,0 +1,11 @@
+from unittest import result
+
+
+int_text = input('give me a integer number')
+int_num = int(int_text)
+
+int_float = input('give me a float number')
+float_num = float(int_float)
+
+result = int_num * float_num
+print('your result is : ', result)
