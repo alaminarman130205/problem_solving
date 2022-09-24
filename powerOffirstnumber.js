3e39d1865471029c573b8c27ee2123487d2ef2c5
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-24 12:39:45 +0600

    power of first number and second number problem solve javascript and python both

---
+++ powerOffirstnumber.js
@@ -0,0 +1,6 @@
+const first_num = 2; 
+const second_num  = 5; 
+
+let power = Math.pow(first_num, second_num);
+
+console.log(' this is the power of ' +first_num+ ' and '+second_num+  ' = ', power);
---
+++ powerofFirstNumber.py
@@ -0,0 +1,9 @@
+import math
+
+
+first_num = int(input('Enter the first number'))
+second_num = int(input('Enter the second number'))
+
+result = math.pow(first_num, second_num)
+
+print('this is the result', result)
