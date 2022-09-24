5290f1284eb2147fad2885755dc11e4ec6b409f7
alaminarmanjanina <81838250+alaminarmanjanina@users.noreply.github.com>
2022-09-24 12:15:39 +0600

    first simple integer and float multiply done

---
+++ integerAndFloadMultiply.js
@@ -0,0 +1,7 @@
+// integer and float number multiply with some basic code
+
+const inte_num = 23; 
+const flo_num  = 23.2323423; 
+
+const float_num_total= inte_num * flo_num; 
+console.log((float_num_total).toFixed(20));
